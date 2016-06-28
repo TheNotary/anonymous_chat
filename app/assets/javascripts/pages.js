a710bf37270c226200995208ee2ce1ee36916fe6
TheNotary <no@email.plz>
2016-06-27 17:05:00 -0700

    stubs out the javascript

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,17 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+function setupClientForFaye() {
+  var client = new Faye.Client(url, {timeout: 120});
+}
+
+function sendMessage(){
+
+
+}
+
+function renderNewMessage(payload){
+
+
+}
